afd0ddad50979106f14e3d18c929227a7a71139b
Roberto Pedroso <roberto@rpedroso.com>
2021-08-20 10:06:14 -0400

    throw together a decent statusline

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -276,7 +276,273 @@ end)
 
 
 -------------------- STATUSLINE ----------------------------
+local gl = require('galaxyline')
+local gls = gl.section
+local condition = require('galaxyline.condition')
 
+local statusline_style = {
+    left = " ",
+    right = " ",
+    main_icon = "  ",
+    vi_mode_icon = " ",
+    position_icon = " ",
+}
+
+local left_separator = statusline_style.left
+local right_separator = statusline_style.right
+
+local colors = {
+    white = "#abb2bf",
+    darker_black = "#2a303c",
+    black = "#2E3440", --  nvim bg
+    black2 = "#343a46",
+    one_bg = "#373d49",
+    one_bg2 = "#464c58",
+    one_bg3 = "#494f5b",
+    grey = "#4b515d",
+    grey_fg = "#565c68",
+    grey_fg2 = "#606672",
+    light_grey = "#646a76",
+    red = "#BF616A",
+    baby_pink = "#de878f",
+    pink = "#d57780",
+    line = "#3a404c", -- for lines like vertsplit
+    green = "#A3BE8C",
+    vibrant_green = "#afca98",
+    blue = "#7797b7",
+    nord_blue = "#81A1C1",
+    yellow = "#EBCB8B",
+    sun = "#e1c181",
+    purple = "#aab1be",
+    dark_purple = "#B48EAD",
+    teal = "#6484a4",
+    orange = "#e39a83",
+    cyan = "#9aafe6",
+    statusline_bg = "#333945",
+    lightbg = "#3f4551",
+    lightbg2 = "#393f4b",
+    pmenu_bg = "#A3BE8C",
+    folder_bg = "#7797b7",
+}
+
+local mode_colors = {
+    [110] = { "NORMAL", colors.red },
+    [105] = { "INSERT", colors.dark_purple },
+    [99] = { "COMMAND", colors.pink },
+    [116] = { "TERMINAL", colors.green },
+    [118] = { "VISUAL", colors.cyan },
+    [22] = { "V-BLOCK", colors.cyan },
+    [86] = { "V_LINE", colors.cyan },
+    [82] = { "REPLACE", colors.orange },
+    [115] = { "SELECT", colors.nord_blue },
+    [83] = { "S-LINE", colors.nord_blue },
+}
+
+local mode = function(n)
+    return mode_colors[vim.fn.mode():byte()][n]
+end
+
+local checkwidth = function()
+   local squeeze_width = vim.fn.winwidth(0) / 2
+   if squeeze_width > 30 then
+      return true
+   end
+   return false
+end
+
+gl.short_line_list = { " " }
+
+gls.left[1] = {
+   FirstElement = {
+      provider = function()
+         return "▋"
+      end,
+      highlight = { colors.nord_blue, colors.nord_blue },
+   },
+}
+
+gls.left[2] = {
+   statusIcon = {
+      provider = function()
+         return statusline_style.main_icon
+      end,
+      highlight = { colors.statusline_bg, colors.nord_blue },
+      separator = right_separator,
+      separator_highlight = { colors.nord_blue, colors.one_bg2 },
+   },
+}
+
+gls.left[3] = {
+   left_arow2 = {
+      provider = function() end,
+      separator = right_separator .. " ",
+      separator_highlight = { colors.one_bg2, colors.lightbg },
+   },
+}
+
+gls.left[4] = {
+   FileIcon = {
+      provider = "FileIcon",
+      condition = condition.buffer_not_empty,
+      highlight = { colors.white, colors.lightbg },
+   },
+}
+
+gls.left[5] = {
+   FileName = {
+      provider = function()
+         local fileinfo = require "galaxyline.provider_fileinfo"
+
+         if vim.api.nvim_buf_get_name(0):len() == 0 then
+            return ""
+         end
+
+         return fileinfo.get_current_file_name("", "")
+      end,
+      highlight = { colors.white, colors.lightbg },
+      separator = right_separator,
+      separator_highlight = { colors.lightbg, colors.lightbg2 },
+   },
+}
+
+gls.left[6] = {
+   current_dir = {
+      provider = function()
+         local dir_name = vim.fn.fnamemodify(vim.fn.getcwd(), ":t")
+         return "  " .. dir_name .. " "
+      end,
+      highlight = { colors.grey_fg2, colors.lightbg2 },
+      separator = right_separator,
+      separator_highlight = { colors.lightbg2, colors.statusline_bg },
+   },
+}
+
+gls.left[7] = {
+   DiffAdd = {
+      provider = "DiffAdd",
+      condition = checkwidth,
+      icon = "  ",
+      highlight = { colors.white, colors.statusline_bg },
+   },
+}
+
+gls.left[8] = {
+   DiffModified = {
+      provider = "DiffModified",
+      condition = checkwidth,
+      icon = "   ",
+      highlight = { colors.grey_fg2, colors.statusline_bg },
+   },
+}
+
+gls.left[9] = {
+   DiffRemove = {
+      provider = "DiffRemove",
+      condition = checkwidth,
+      icon = "  ",
+      highlight = { colors.grey_fg2, colors.statusline_bg },
+   },
+}
+
+gls.left[10] = {
+   DiagnosticError = {
+      provider = "DiagnosticError",
+      icon = "  ",
+      highlight = { colors.red, colors.statusline_bg },
+   },
+}
+
+gls.left[11] = {
+   DiagnosticWarn = {
+      provider = "DiagnosticWarn",
+      icon = "  ",
+      highlight = { colors.yellow, colors.statusline_bg },
+   },
+}
+
+gls.right[1] = {
+   lsp_status = {
+      provider = function()
+         local clients = vim.lsp.get_active_clients()
+         if next(clients) ~= nil then
+            local buf_ft = vim.api.nvim_buf_get_option(0, "filetype")
+            for _, client in ipairs(clients) do
+               local filetypes = client.config.filetypes
+               if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
+                  return " " .. "  " .. " LSP"
+               end
+            end
+            return ""
+         else
+            return ""
+         end
+      end,
+      highlight = { colors.grey_fg2, colors.statusline_bg },
+   },
+}
+
+gls.right[2] = {
+   GitIcon = {
+      provider = function()
+         return "  "
+      end,
+      condition = require("galaxyline.condition").check_git_workspace,
+      highlight = { colors.grey_fg2, colors.statusline_bg },
+      separator = " ",
+      separator_highlight = { colors.statusline_bg, colors.statusline_bg },
+   },
+}
+
+gls.right[3] = {
+   GitBranch = {
+      provider = "GitBranch",
+      condition = require("galaxyline.condition").check_git_workspace,
+      highlight = { colors.grey_fg2, colors.statusline_bg },
+   },
+}
+gls.right[4] = {
+    space = {
+        provider = function() return " " end,
+        highlight = { colors.grey_fg2, colors.statusline_bg },
+    }
+}
+
+gls.right[5] = {
+   ViMode = {
+      provider = function()
+         vim.cmd("hi GalaxyViMode guifg=" .. mode(2))
+         return " " .. mode(1) .. " "
+      end,
+      highlight = { "GalaxyViMode", colors.lightbg2 },
+      separator = right_separator,
+      separator_highlight = { colors.statusline_bg, colors.lightbg2 },
+   },
+}
+
+gls.right[6] = {
+   line_percentage = {
+      provider = function()
+        local current_line = vim.fn.line "."
+        local total_line = vim.fn.line "$"
+
+        if current_line == 1 then
+            return " Top  "
+        elseif current_line == vim.fn.line "$" then
+            return " Bot  "
+        end
+
+        local result, _ = math.modf((current_line / total_line) * 100)
+        if result < 10 then
+            return "  " .. result .. "%  "
+        else
+            return " " .. result .. "%  "
+        end
+      end,
+      highlight = { colors.green, colors.lightbg },
+      separator = right_separator,
+      separator_highlight = { colors.lightbg2, colors.lightbg },
+   },
+}
 
 -------------------- MAPPINGS ------------------------------
 map('n', '<leader>n', ':NvimTreeToggle<CR>')
