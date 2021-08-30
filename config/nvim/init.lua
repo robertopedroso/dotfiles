b3d5c13cc74976b9a96ca31c8cba53efaf5858e8
Roberto Pedroso <roberto@rpedroso.com>
2021-08-30 16:02:15 -0400

    we use statusline for mode display

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -46,6 +46,7 @@ opt.scrolloff = 4                   -- Lines of context
 opt.shiftround = true               -- Round indent
 opt.shiftwidth = indent             -- Size of an indent
 opt.shortmess = 'atToOFc'           -- Prompt message options
+opt.showmode = false                -- We use the statusline for this instead
 opt.sidescrolloff = 8               -- Columns of context
 opt.signcolumn = 'yes'              -- Show sign column
 opt.smartcase = true                -- Do not ignore case with capitals
